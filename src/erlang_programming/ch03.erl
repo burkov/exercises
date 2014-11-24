940e036ebf9113114d84feb59f6068f0b4a16c9f
Alexander Burkov <alex.burkov@motorolasolutions.com>
2014-11-25 00:34:38 +0300

    Add stub for chapter 3

--- src/erlang_programming/ch03.erl
+++ src/erlang_programming/ch03.erl
@@ -3,12 +3,12 @@
 
 -compile(export_all).
 
--include_lib("eqc/include/eqc.hrl").
+-include_lib("proper/include/proper.hrl").
 
 
 
 sum(1) -> 1;
-sum(N) when N > 1 -> sum(N-1) + N.
+sum(N) when N > 1 -> sum(N - 1) + N.
 
 
 sum(M, M) -> M;
@@ -17,6 +17,11 @@ sum(N, M) when N > M -> exit(badarg).
 
 
 prop_sum1() ->
-  ?FORALL(X, eqc_gen:)
+  ?FORALL(X, proper_types:choose(1, 1024), lists:sum(lists:seq(1, X)) =:= ch03:sum(X)).
+
+prop_sum2() ->
+  ?FORALL({N, M}, ?SUCHTHAT({N, M}, {proper_types:choose(1, 1024), proper_types:choose(1, 1024)}, N =< M),
+    lists:sum(lists:seq(N, M)) =:= ch03:sum(N, M)).
+
 
 
