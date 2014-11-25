85f1c6e0dc7fa65f7c4c97adab5f40d39dd7d2c2
Alexander Burkov <alex.burkov@motorolasolutions.com>
2014-11-25 12:22:03 +0300

    Migrate from EQC to proper

--- src/erlang_programming/ch02.erl
+++ src/erlang_programming/ch02.erl
@@ -1,40 +1,40 @@
 -module(ch02).
 -compile(export_all).
 
--include_lib("eqc/include/eqc.hrl").
+-include_lib("proper/include/proper.hrl").
 
 b_and(false, false) -> false;
 b_and(true, true) -> true;
 b_and(false, true) -> false;
 b_and(true, false) -> false;
-b_and(_,_) -> error(badarg).
+b_and(_, _) -> error(badarg).
 
 b_or(true, false) -> true;
 b_or(false, true) -> true;
 b_or(true, true) -> true;
 b_or(false, false) -> false;
-b_or(_,_) -> error(badarg).
+b_or(_, _) -> error(badarg).
 
 b_not(true) -> false;
 b_not(false) -> true;
 b_not(_) -> error(badarg).
 
-b_nand(A, B) -> b_not(b_and(A,B)).
+b_nand(A, B) -> b_not(b_and(A, B)).
 
 
 %%% tests
 
 test() ->
-    eqc:module([{numtests, 1024}], ?MODULE).
+  proper:module(?MODULE, [{numtests, 1024}]).
 
 prop_b_not() ->
-    ?FORALL(Bool, eqc_gen:bool(), equals(b_not(Bool), not Bool)).
+  ?FORALL(Bool, proper_types:bool(), proper:equals(b_not(Bool), not Bool)).
 
 prop_b_or() ->
-    ?FORALL({A, B}, {eqc_gen:bool(), eqc_gen:bool()}, equals(b_or(A,B), A or B)).
+  ?FORALL({A, B}, {proper_types:bool(), proper_types:bool()}, proper:equals(b_or(A, B), A or B)).
 
 prop_b_and() ->
-    ?FORALL({A, B}, {eqc_gen:bool(), eqc_gen:bool()}, equals(b_and(A,B), A and B)).
+  ?FORALL({A, B}, {proper_types:bool(), proper_types:bool()}, proper:equals(b_and(A, B), A and B)).
 
 prop_b_nand() ->
-    ?FORALL({A, B}, {eqc_gen:bool(), eqc_gen:bool()}, equals(b_nand(A,B), not (A and B))).
+  ?FORALL({A, B}, {proper_types:bool(), proper_types:bool()}, proper:equals(b_nand(A, B), not (A and B))).
